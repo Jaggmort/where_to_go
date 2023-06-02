0fb1f3477e63b79d25afe01b53a524582ee8c65a
Jaggmort <66931312+Jaggmort@users.noreply.github.com>
2023-06-02 13:10:09 +0300

    Added Images inline to Places admin

--- places/admin.py
+++ places/admin.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 
 from .models import Places, Images
 
+class BookInline(admin.TabularInline):
+    model = Images
+
 @admin.register(Places)
 class PlacesAdmin(admin.ModelAdmin):
     list_display = ['title', 'lng', 'lat']
+    inlines = [
+        BookInline,
+    ]
 
 
 @admin.register(Images)
