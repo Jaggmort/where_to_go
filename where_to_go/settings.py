ddb27ff1803e387739c27dbe4fcb4ee1281e20ed
Jaggmort <66931312+Jaggmort@users.noreply.github.com>
2023-06-02 12:28:55 +0300

    Fixed media

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -118,14 +118,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = 'media/'
-MEDIAFILES_DIR = (
-    os.path.join(BASE_DIR, 'media'),
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = (
+ 
+os.path.join(BASE_DIR, 'assets'),
+ 
 )
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
