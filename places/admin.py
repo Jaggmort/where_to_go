f54001884446232de43ad2c76c45320dea8f9568
Jaggmort <66931312+Jaggmort@users.noreply.github.com>
2023-06-02 15:15:53 +0300

    Added WYSIWYG-editor

--- places/admin.py
+++ places/admin.py
@@ -2,23 +2,23 @@ from django.contrib import admin
 from .models import Places, Images
 from django.contrib import admin
 from django.utils.html import format_html
-from adminsortable2.admin import SortableTabularInline
-from adminsortable2.admin import SortableAdminMixin
+from adminsortable2.admin import SortableTabularInline, SortableAdminBase
 
-class BookInline(SortableTabularInline):
+class BookTabularInline(SortableTabularInline):
     model = Images
-    fields = ['image', 'pl_image', 'sequence_number']
+    fields = ('image', 'pl_image', 'sequence_number')
     readonly_fields = ('pl_image', )
+    extra = 0
 
     def pl_image(self, image_object):
         return format_html('<img src="{}" height="200px" width="200px" />', image_object.image.url)
 
 @admin.register(Places)
-class PlacesAdmin(SortableAdminMixin, admin.ModelAdmin):
-    list_display = ['sequence_number', 'title', 'lng', 'lat']
+class PlacesAdmin(SortableAdminBase, admin.ModelAdmin):
+    list_display = ['title', 'lng', 'lat']
 
     inlines = [
-        BookInline,
+        BookTabularInline,
     ]
 
 
--- places/models.py
+++ places/models.py
@@ -1,17 +1,16 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 class Places(models.Model):
     title = models.CharField('Заголовок', max_length=80)
     description_short = models.CharField('Короткое описание', max_length=400, blank=True)
-    description_long = models.TextField('Длинное описание', blank=True)
+    description_long = HTMLField('Длинное описание', blank=True)
     lng = models.FloatField('Долгота', blank=True)
     lat = models.FloatField('Широта', blank=True)
-    sequence_number = models.PositiveIntegerField('Индекс в списке интересных мест', default=0, blank=True, null=True)
 
     class Meta:
         verbose_name = 'Интересное место'
         verbose_name_plural = 'Интересные места'
-        ordering = ['sequence_number']
 
     def __str__(self):
         return f'{self.title}'
